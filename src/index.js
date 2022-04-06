5e50cf1c3f6e53307e92bfd1cef15dd890a84678
Rinakae <simi1993@mail.ru>
2022-04-06 19:52:38 +0300

    change task6, remove into modal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import modal from './modules/modal';
 import menu from './modules/menu';
 import slider from './modules/slider';
-import callBack from './modules/callBack';
 
 modal();
 menu();
 slider();
-callBack();
+
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,5 +1,5 @@
 const modal = () => {
-  const callbackBtn = document.querySelectorAll(".callback-btn");
+  const callbackBtn = document.querySelectorAll(".callback-btn, .button-services");
   const modalCallback = document.querySelector(".modal-callback");
   const modalOverlay = document.querySelector(".modal-overlay");
   const modalClose = document.querySelector(".modal-close");
