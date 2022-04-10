630417c6d99d8d31b546780cd9a4b0ca1485aeb5
Rinakae <simi1993@mail.ru>
2022-04-10 21:02:04 +0300

    add carousel

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import modal from './modules/modal';
 import menu from './modules/menu';
 import slider from './modules/slider';
 import accordion from './modules/accordion';
+import carousel from './modules/carousel';
 
 modal();
 menu();
 slider();
 accordion();
+carousel();
 
---
+++ src/modules/carousel.js
@@ -0,0 +1,12 @@
+const carousel = () => {
+  const carousel = document.querySelector(".services-elements");
+  const ul = document.querySelector(".services-carousel");
+  const li = ul.querySelectorAll(".col-sm-6");
+  const left = document.querySelector(".arrow-left");
+  const right = document.querySelector(".arrow-right");
+
+  
+ 
+};
+
+export default carousel;
