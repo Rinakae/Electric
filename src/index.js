e94c18e8714dc0302b28987dff595708b3cc6891
Rinakae <simi1993@mail.ru>
2022-04-11 21:40:28 +0300

    create sendForm

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import menu from './modules/menu';
 import slider from './modules/slider';
 import accordion from './modules/accordion';
 import scrollUp from './modules/scrollUp';
+import sendForm from './modules/sendForm';
 
 modal();
 menu();
 slider();
 accordion();
 scrollUp();
+sendForm();
 
