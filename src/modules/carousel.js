b80417529e64cd5dccd555f469d69d511e61cf47
Rinakae <simi1993@mail.ru>
2022-04-13 00:26:45 +0300

    add carousel move

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -1,11 +1,31 @@
+"use strict";
 const carousel = () => {
   const carousel = document.querySelector(".services-elements");
-  const ul = document.querySelector(".services-carousel");
-  const li = ul.querySelectorAll(".col-sm-6");
+  const list = carousel.querySelector(".services-carousel");
+  const listElems = carousel.querySelectorAll('.col-sm-6');  
   const left = document.querySelector(".arrow-left");
   const right = document.querySelector(".arrow-right");
 
+  const width = 400; 
+  let count = 1;
+  let position = 0;
   
+  let i = 0;
+  for(let item of carousel.querySelectorAll('.col-sm-6')) {
+      item.style.position = 'relative';     
+      i++;
+    }
+  left.addEventListener("click", (e) => {        
+    position += width * count;      
+    position = Math.min(position, 0);
+    list.style.marginLeft = position + 'px';    
+  });
+
+  right.addEventListener("click", (e) => {
+    position -= width * count;      
+    position = Math.max(position, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';     
+  }); 
  
 };
 
